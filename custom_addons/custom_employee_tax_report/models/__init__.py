403ec8649573444731f82bc4e0fb2c66f0647667
zerabruck <zerabruckm@gmail.com>
2024-10-30 21:53:25 +0300

    add tax report functionality

--- custom_addons/custom_employee_tax_report/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import employee_tax_report
--- custom_addons/om_hr_payroll/models/employee_tax_report.py
+++ custom_addons/om_hr_payroll/models/employee_tax_report.py
@@ -1,19 +1,39 @@
 from odoo import models, api
 
 class EmployeeTaxReport(models.AbstractModel):
-    _name = 'report.custom_employee_tax_report.report_employee_tax_template'
+    _name = 'report.om_hr_payroll.report_employee_tax_template'
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        # Fetch the current company (you can adjust this to fetch any specific company if needed)
         company = self.env.company
         employees = self.env['hr.employee'].search([])  # Get all employees
         count = 1
         employee_tax_data = []
         current_batch = []
+        end_date_employee = []
+        month = ''
+        year = ''
+        first_payslip_date = None
+        if employees:
+            first_employee = employees[1]
+            payslips = self.env['hr.payslip'].search([('employee_id', '=', first_employee.id)], limit=1,
+                                                     order='date_from ASC')
+            if payslips:
+                first_payslip_date = payslips.date_from
+                month = first_payslip_date.strftime('%b').upper()  # E.g., "FEB", "NOV"
+                year = first_payslip_date.strftime('%Y')
+            else:
+                print("No payslips found for the first employee.")
 
         for employee in employees:
-            contract = employee.contract_id  # Get the employee's contract
+            contract = employee.contract_id  
+            if(contract.date_end and contract.date_end.strftime('%b').upper() == month and contract.date_end.strftime('%Y') == year):
+                end_date_employee.append({
+                    "no": count,
+                    'tin_number': employee.tin_number,
+                    'name': employee.legal_name,
+                })
+
             current_batch.append({
                 "no": count,
                 'tin_number': employee.tin_number,
@@ -21,7 +41,7 @@ class EmployeeTaxReport(models.AbstractModel):
                 'start_date': employee.first_contract_date,
                 'basic_salary': contract.wage,
                 'transport_allowance': contract.travel_allowance,
-                'taxable_transport_allowance': "",  # Adjust this as needed
+                'taxable_transport_allowance': "",  
                 'over_time': "",
                 'other_taxable_benefits': contract.meal_allowance + contract.medical_allowance + contract.other_allowance,
                 'taxable_income': "",
@@ -32,16 +52,18 @@ class EmployeeTaxReport(models.AbstractModel):
 
             count += 1
 
-            # If the current batch reaches 20, append it to employee_tax_data and reset
-            if len(current_batch) == 1:
+            if len(current_batch) == 7:
                 employee_tax_data.append(current_batch)
-                current_batch = []  # Reset the current batch
+                current_batch = []  
 
-        # Append any remaining employees in the last batch
         if current_batch:
             employee_tax_data.append(current_batch)
 
         return {
-            'company': company,  # Pass the real company object
-            'employees': employee_tax_data,  # Pass employee info as an array of arrays
+            'company': company,  
+            'employees': employee_tax_data,  
+            'month': month,
+            'year':year,
+            'end_date_employee':end_date_employee,
+            'first_payslip_date':first_payslip_date
         }
