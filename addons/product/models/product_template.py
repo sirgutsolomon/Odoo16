7b94f0a04c0539067f2927304c22dbb52d2fcf65
Tsedeniya <tamare@umich.edu>
2025-01-14 12:01:26 +0300

    new changes

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -88,7 +88,7 @@ class ProductTemplate(models.Model):
 
     # list_price: catalog price, user defined
     list_price = fields.Float(
-        'Sales Price', default=1.0,
+        'Sales Price', default=0.0,
         digits='Product Price',
         tracking=True,
         help="Price at which the product is sold to customers.",
